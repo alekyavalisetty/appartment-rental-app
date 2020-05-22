1b3cacc116333a63eb2f575c63b579c462a734ee
alekyavalisetty <alekyavalisetty@gmail.com>
2020-05-21 21:49:02 -0400

    login activity

--- app/src/main/java/com/pramod/apartmentrental/Login_activity.java
+++ app/src/main/java/com/pramod/apartmentrental/Login_activity.java
@@ -2,13 +2,40 @@ package com.pramod.apartmentrental;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login_activity extends AppCompatActivity {
+
+
+    EditText mLoginEmail, mLoginPassword;
+    Button mLogin;
+    TextView mSignup, mForgetPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_login );
+
+        mLoginEmail = findViewById(R.id.et_email);
+        mLoginPassword = findViewById(R.id.et_password);
+        mLogin = findViewById(R.id.btn_login);
+
+        mSignup = findViewById(R.id.tv_signup);
+        mForgetPassword = findViewById(R.id.tv_forget_pwd);
+
+        mSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login_activity.this, Signup_activity.class);
+                //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
     }
 }
