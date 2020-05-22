b87be9f8ed4151c4f70f477ce8725fe67e9f6d67
sagardhawan99 <sagardhawansd54@gmail.com>
2020-05-21 22:04:37 -0400

    Added click listeners for signup. Made changes according to last week comments

--- app/src/main/java/com/pramod/apartmentrental/Signup_activity.java
+++ app/src/main/java/com/pramod/apartmentrental/Signup_activity.java
@@ -3,10 +3,12 @@ package com.pramod.apartmentrental;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Signup_activity extends AppCompatActivity {
 
@@ -14,6 +16,7 @@ public class Signup_activity extends AppCompatActivity {
     ImageView mPhoto;
     Spinner mSpinner;
     Button mSignup;
+    TextView mBackBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,14 @@ public class Signup_activity extends AppCompatActivity {
         mPhone = findViewById(R.id.et_name);
         mPhoto = findViewById(R.id.photo);
         mSignup = findViewById(R.id.btn_signup);
+        mBackBtn = findViewById(R.id.back);
+
+        mBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
 
 
