2406bd2912f201c55792a2f588de304ef73485c2
sukhdeepsinghbrar <Sukhdeepbrar.jw@gmail.com>
2020-05-23 12:53:39 -0400

    Selection Screen, Renter account and add fragment init

---
+++ app/src/main/java/com/pramod/apartmentrental/SelectionScreen_activity.java
@@ -0,0 +1,14 @@
+package com.pramod.apartmentrental;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectionScreen_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection_screen_activity);
+    }
+}
