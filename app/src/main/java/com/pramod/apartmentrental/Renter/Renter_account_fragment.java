3e7c921165aee611ba34d678cfd5a7d8843b30ec
alekyavalisetty <alekyavalisetty@gmail.com>
2020-05-23 12:14:04 -0400

    fragment layouts for renter account and renter add apartments

---
+++ app/src/main/java/com/pramod/apartmentrental/Renter/Renter_account_fragment.java
@@ -0,0 +1,29 @@
+package com.pramod.apartmentrental.Renter;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pramod.apartmentrental.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Renter_account_fragment extends Fragment {
+
+    public Renter_account_fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_renter_account_fragment, container, false);
+    }
+}
