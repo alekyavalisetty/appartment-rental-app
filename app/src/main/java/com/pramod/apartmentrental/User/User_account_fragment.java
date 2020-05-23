23dd412235a6576b3cf08d88074d8c8142030b3c
akvinderkaur <akvinderkaur01021994@gmail.com>
2020-05-23 11:56:46 -0400

    Created account and favourites fragment for user

---
+++ app/src/main/java/com/pramod/apartmentrental/User/User_account_fragment.java
@@ -0,0 +1,31 @@
+package com.pramod.apartmentrental.User;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.pramod.apartmentrental.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class User_account_fragment extends Fragment {
+
+    public User_account_fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+}
