28fb2adce034f79fa4f06d0eb29aae59cc9f6371
sagardhawan99 <sagardhawansd54@gmail.com>
2020-05-25 22:23:28 -0400

    Firebase Signup Query with all the inputs. Updating the firebase database.

--- app/src/main/java/com/pramod/apartmentrental/Signup_activity.java
+++ app/src/main/java/com/pramod/apartmentrental/Signup_activity.java
@@ -1,20 +1,44 @@
 package com.pramod.apartmentrental;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.SignInMethodQueryResult;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Signup_activity extends AppCompatActivity {
 
@@ -24,10 +48,12 @@ public class Signup_activity extends AppCompatActivity {
     Spinner mSpinner;
     Button mSignup;
     TextView mBackBtn;
+    //Setting up role initially
+    private String role, userImageUrl = " ";
 
     //Firebase variables for session checking
     FirebaseAuth mFirebaseAuth;
-    private Uri resultUri;
+    private Uri mResultUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +65,10 @@ public class Signup_activity extends AppCompatActivity {
 
 
         mName = findViewById(R.id.et_name);
-        mEmail = findViewById(R.id.et_name);
-        mPassword = findViewById(R.id.et_name);
-        mPhone = findViewById(R.id.et_name);
+        mEmail = findViewById(R.id.et_email);
+        mPassword = findViewById(R.id.et_password);
+        mPhone = findViewById(R.id.et_phone);
+
         mPhoto = findViewById(R.id.photo);
         mSignup = findViewById(R.id.btn_signup);
         mBackBtn = findViewById(R.id.back);
@@ -52,6 +79,23 @@ public class Signup_activity extends AppCompatActivity {
 
         mRoleAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mSpinner.setAdapter(mRoleAdapter);
+
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position ==0)
+                {
+                    role = "user";
+                }
+                else {
+                    role = "admin";
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
 
         mBackBtn.setOnClickListener(new View.OnClickListener() {
@@ -77,44 +121,156 @@ public class Signup_activity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                final String name = mName.getText().toString();
-                final String email = mEmail.getText().toString();
-                final String password = mPassword.getText().toString();
-                final String phone = mPhone.getText().toString();
-
-                if (TextUtils.isEmpty(name)) {
-                    mName.setError("Please enter your First Name");
+                if (TextUtils.isEmpty(mName.getText().toString())) {
+                    mName.setError("Please enter your Name");
                 }
-                else if(TextUtils.isEmpty(email))
+                else if(TextUtils.isEmpty(mEmail.getText().toString()))
                 {
                     mEmail.setError("Please enter your Email ID");
                 }
-                else if(!email.contains("@") || (!email.contains(".")))
-                {
-                    mEmail.setError("Please enter a valid Email address");
+                else if (TextUtils.isEmpty(mPassword.getText().toString())) {
+                    mPassword.setError("Please enter your password");
                 }
-                else if (TextUtils.isEmpty(password)) {
-                    mPhone.setError("Please enter your password");
-                }
-                else if(password.length() < 6){
+                else if(mPassword.getText().toString().length() < 6){
                     mPassword.setError("Password length is minimum 6 characters.");
                 }
-                else if(TextUtils.isEmpty(phone))
-                {
+                else if(TextUtils.isEmpty(mPhone.getText().toString())) {
                     mPhone.setError("Please enter your phone number");
                 }
-                else if(phone.length() < 10){
-
+                else if(mPhone.getText().toString().length() < 10) {
                     mPhone.setError("Please enter a valid phone number");
                 }
                 else {
-                    //Database Registration Query
 
+                    final String name = mName.getText().toString();
+                    final String email = mEmail.getText().toString();
+                    final String password = mPassword.getText().toString();
+                    final String phone = mPhone.getText().toString();
+                    role = mSpinner.getSelectedItem().toString().trim();
+                    //Database Registration Query
+                    checkIfEmailAlreadyExists(email);
+
+                    mFirebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            final Map userDetails = new HashMap<>();
+
+                            if(task.isSuccessful()){
+
+                                String currentUserId = mFirebaseAuth.getCurrentUser().getUid();
+
+                                //creates a new user id with all the data in realtime database
+                                final DatabaseReference currentUserDbReference = FirebaseDatabase.getInstance().getReference()
+                                        .child("users").child(currentUserId);
+
+                                if(mResultUri != null) {
+
+                                    StorageReference filepath = FirebaseStorage.getInstance().getReference().child("user_photos")
+                                            .child(currentUserId);
+
+                                    Bitmap bitmap = null;
+
+                                    try {
+                                        bitmap = MediaStore.Images.Media.getBitmap(getApplication().getContentResolver(), mResultUri);
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                    //Reduce image size
+                                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                                    bitmap.compress(Bitmap.CompressFormat.JPEG, 20, baos);
+
+                                    byte[] data = baos.toByteArray();
+
+                                    //Upload the image
+                                    UploadTask uploadTask = filepath.putBytes(data);
+
+                                    uploadTask.addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            finish();
+                                        }
+                                    });
+
+                                    uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                        @Override
+                                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                                            if (taskSnapshot.getMetadata() != null) {
+                                                if (taskSnapshot.getMetadata().getReference() != null) {
+                                                    Task<Uri> result = taskSnapshot.getStorage().getDownloadUrl();
+                                                    result.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                                        @Override
+                                                        public void onSuccess(Uri uri) {
+                                                            userImageUrl = uri.toString();
+                                                            userDetails.put("photo",userImageUrl);
+                                                            currentUserDbReference.updateChildren(userDetails);
+                                                        }
+                                                    });
+                                                }
+                                            }
+                                        }
+                                    });
+
+                            }
+
+                                userDetails.put("name",name);
+                                userDetails.put("email",email);
+                                userDetails.put("role",role);
+                                userDetails.put("phone",phone);
+
+                                currentUserDbReference.updateChildren(userDetails);
+                                Toast.makeText(Signup_activity.this, "Sign up is successful. Redirecting to selection screen", Toast.LENGTH_SHORT).show();
+
+                                Intent i = new Intent(Signup_activity.this, SelectionScreen_activity.class);
+                                startActivity(i);
+                                finish();
+
+                            }
+                        }
+                    });
                 }
 
             }
         });
 
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //if the intent from activity code is 1
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+
+            final Uri imageUri = data.getData();
+            mResultUri = imageUri;
+            mPhoto.setImageURI(mResultUri);
+        }
+    }
+
+    private void checkIfEmailAlreadyExists(String email) {
+
+        mFirebaseAuth.fetchSignInMethodsForEmail(this.mEmail.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
+
+                        boolean check = !task.getResult().getSignInMethods().isEmpty();
+
+                        if(!check)
+                        {
+                            Log.d("Message","Email dont exist.");
+
+                        }
+                        else
+                        {
+                            mEmail.setError("Email is already registered. Try another one.");
+                        }
+                    }
+                });
 
     }
 }
