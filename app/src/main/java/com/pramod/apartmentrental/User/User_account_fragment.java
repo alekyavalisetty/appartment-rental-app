d07dc0bb87bb82b7b4412dd95f1f60971d5d14f7
Meratamasha1 <3s.sagar@gmail.com>
2020-05-26 21:20:42 -0400

    Changed layout of useraccount fragment

--- app/src/main/java/com/pramod/apartmentrental/User/User_account_fragment.java
+++ app/src/main/java/com/pramod/apartmentrental/User/User_account_fragment.java
@@ -24,8 +24,6 @@ public class User_account_fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        return inflater.inflate(R.layout.fragment_user_account_fragment, container, false);
     }
 }
