fe20a0111720eecf5daffb4209da9465a6274c94
Meratamasha1 <3s.sagar@gmail.com>
2020-05-26 21:19:46 -0400

    Signout and toolbars link in user and renter dash

--- app/src/main/java/com/pramod/apartmentrental/Renter/RenterDashboard_activity.java
+++ app/src/main/java/com/pramod/apartmentrental/Renter/RenterDashboard_activity.java
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.pramod.apartmentrental.Login_activity;
 import com.pramod.apartmentrental.R;
 import com.pramod.apartmentrental.User.User_account_fragment;
 import com.pramod.apartmentrental.User.User_favourites_fragment;
@@ -18,6 +24,8 @@ import com.pramod.apartmentrental.User.User_maps_fragment;
 public class RenterDashboard_activity extends AppCompatActivity {
 
     private BottomNavigationView UserNavigation;
+    private FirebaseAuth mFirebaseAuth;
+    MaterialToolbar materialToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +33,22 @@ public class RenterDashboard_activity extends AppCompatActivity {
         setContentView(R.layout.activity_renter_dashboard_activity);
 
         UserNavigation = findViewById(R.id.bottom_nav_view_renter);
+        materialToolbar = findViewById(R.id.toolbar);
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        setSupportActionBar(materialToolbar);
         UserNavigation.setOnNavigationItemSelectedListener(mOnNavigationClickListener);
-        //Initialise user home fragment
         UserNavigation.setSelectedItemId(R.id.home);
+
+
+    }
+
+    private void signOut() {
+        mFirebaseAuth.signOut();
+        Intent loginIntent = new Intent(this, Login_activity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(loginIntent);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationClickListener
@@ -59,4 +79,24 @@ public class RenterDashboard_activity extends AppCompatActivity {
         transaction.replace(R.id.container, fragment);
         transaction.commit();
     }
+
+    //Toolbar options
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        switch (id){
+            case R.id.toolbar_signout:
+                Toast.makeText(this, "Signed out successfully", Toast.LENGTH_SHORT).show();
+                signOut();
+                return true;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
--- app/src/main/java/com/pramod/apartmentrental/User/UserDashboard_activity.java
+++ app/src/main/java/com/pramod/apartmentrental/User/UserDashboard_activity.java
@@ -5,15 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.pramod.apartmentrental.Login_activity;
 import com.pramod.apartmentrental.R;
 
 public class UserDashboard_activity extends AppCompatActivity {
 
     private BottomNavigationView UserNavigation;
+    private FirebaseAuth mFirebaseAuth;
+    MaterialToolbar materialToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +29,23 @@ public class UserDashboard_activity extends AppCompatActivity {
         setContentView(R.layout.activity_user_dashboard_activity);
 
         UserNavigation = findViewById(R.id.bottom_nav_view_user);
+        materialToolbar = findViewById(R.id.toolbar);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
         UserNavigation.setOnNavigationItemSelectedListener(mOnNavigationClickListener);
         //Initialise user home fragment
+        setSupportActionBar(materialToolbar);
         UserNavigation.setSelectedItemId(R.id.home);
 
     }
 
+    private void signOut() {
+        mFirebaseAuth.signOut();
+        Intent loginIntent = new Intent(this, Login_activity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(loginIntent);
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationClickListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -57,4 +75,23 @@ public class UserDashboard_activity extends AppCompatActivity {
         transaction.replace(R.id.container, fragment);
         transaction.commit();
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        switch (id){
+            case R.id.toolbar_signout:
+                Toast.makeText(this, "Signed out successfully", Toast.LENGTH_SHORT).show();
+                signOut();
+                return true;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
