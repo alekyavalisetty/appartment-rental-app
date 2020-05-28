bb75c04e5ee94471f97fc9d2b86ab30c5babcfc0
sagardhawan99 <sagardhawansd54@gmail.com>
2020-05-27 22:45:56 -0400

    Get current location for location field

--- app/src/main/java/com/pramod/apartmentrental/Renter/Renter_add_apartment_fragment.java
+++ app/src/main/java/com/pramod/apartmentrental/Renter/Renter_add_apartment_fragment.java
@@ -1,8 +1,10 @@
 package com.pramod.apartmentrental.Renter;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -16,6 +18,9 @@ import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,15 +31,25 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.pramod.apartmentrental.Login_activity;
 import com.pramod.apartmentrental.R;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
@@ -55,9 +70,7 @@ public class Renter_add_apartment_fragment extends Fragment {
     String imageUrl;
 
     EditText listingName, listingDescription,  listingPrice, listingLocation1;
-
-    EditText listingBathrooms, listingBedrooms;
-    ImageButton bedAdd, bedRemove, bathAdd, bathRemove, getLocation;
+    ImageButton getLocation;
     Button postButton;
     AutoCompleteTextView listingLocation;
 
@@ -106,6 +119,7 @@ public class Renter_add_apartment_fragment extends Fragment {
         listingPrice = view.findViewById(R.id.listing_price);
         listingImage = view.findViewById(R.id.listing_image);
         getLocation = view.findViewById(R.id.button_get_location);
+        postButton = getActivity().findViewById(R.id.button_post_ad);
 
 
         //to select image from galary.
@@ -121,8 +135,140 @@ public class Renter_add_apartment_fragment extends Fragment {
             }
         });
 
+        //Get current location
+        getLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-       
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ActivityCompat.checkSelfPermission(getContext(),
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1000);
+
+                } else {
+                    LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+                    Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    Geocoder gcd = new Geocoder(getContext(), Locale.getDefault());
+
+                    List<Address> addresses;
+
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+
+                    try {
+
+                        addresses = gcd.getFromLocation(latitude,longitude ,1);
+
+                        if (addresses.size() >0) {
+                            address = addresses.get(0).getAddressLine(0);
+                            city = addresses.get(0).getLocality();
+
+                            listingLocation.setText(address);
+                        }
+
+                    }catch (Exception e)
+                    {
+                        Toast.makeText(getContext(), "Location not Found!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+
+        postButton.setOnClickListener(new View.OnClickListener() {
+                ShowDialog showDialog = new ShowDialog();
+getActivity()
+                showDialog.show(getSupportFragmentManager(),.
+            @Override
+            public void onClick(View view) {
+
+                aptName = listingName.getText().toString();
+                aptDescription = listingDescription.getText().toString();
+                aptLocation = listingLocation.getText().toString();
+                aptLocation1 = listingLocation1.getText().toString();
+                aptPrice = listingPrice.getText().toString();
+
+                Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
+
+                try {
+                    List<Address> addresses = geocoder.getFromLocationName(aptLocation, 1);
+
+                    if (addresses.size() > 0) {
+                        latitude = (addresses.get(0).getLatitude());
+                        longitude = (addresses.get(0).getLongitude());
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                //Create a new map to post details
+                Map listingInfo = new HashMap();
+                if (TextUtils.isEmpty(aptName))
+                {
+                    listingName.setError("Enter Apartment name");
+                }
+                else if (TextUtils.isEmpty(aptDescription))
+                {
+                    listingDescription.setError("Enter apartment description");
+                }
+                else if (TextUtils.isEmpty(aptLocation))
+                {
+                    listingLocation.setError("Please enter the address");
+                }
+                else if(TextUtils.isEmpty(aptPrice))
+                {
+                    listingPrice.setError("Please enter the rent amount");
+                }
+                else{
+
+                    //for list id
+                    key = FirebaseDatabase.getInstance().getReference().child("listings").push().getKey();
+                    listingInfo.put("listing_id", key);
+                    listingInfo.put("listing_name",aptName);
+                    listingInfo.put("listing_description",aptDescription);
+                    listingInfo.put("listing_location",aptLocation);
+                    listingInfo.put("listing_location1",aptLocation1);
+                    listingInfo.put("listing_price",aptPrice);
+                    listingInfo.put("listing_owner_id",currentUId);
+                    listingInfo.put("listing_city", city);
+                    listingInfo.put("listing_latitude",latitude);
+                    listingInfo.put("listing_longitude", longitude);
+
+
+
+                   
+
+                    } else {
+
+                        imageUrl = "default";
+
+                        listingInfo.put("listing_image", imageUrl);
+                        mListingDatabase.child(key).updateChildren(listingInfo);
+                    }
+                } "Posting Dialog");
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //if the intent from activity code is 1
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+
+            final Uri imageUri = data.getData();
+            resultUri = imageUri;
+            listingImage.setImageURI(resultUri);
+
+        }
+    }
+
+    public void currentRefreshFragment(Fragment fragment){
+
+        getFragmentManager().beginTransaction().detach(fragment).attach(fragment).commit();
 
     }
 }
