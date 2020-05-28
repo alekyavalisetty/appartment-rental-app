41cdbea60bd9f26a20684a672f42767fea5b73f9
sagardhawan99 <sagardhawansd54@gmail.com>
2020-05-28 15:45:22 -0400

    Setup renter listings with recycler view

---
+++ app/src/main/java/com/pramod/apartmentrental/Renter/my_listings/Renter_my_listings.java
@@ -0,0 +1,98 @@
+package com.pramod.apartmentrental.Renter.my_listings;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.pramod.apartmentrental.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Renter_my_listings extends Fragment {
+
+    ImageView listImage;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mListingAdapter;
+    private RecyclerView.LayoutManager mListingLayoutManager;
+    private String currentUserID;
+
+    public Renter_my_listings() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_renter_my_listings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        currentUserID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+
+        mRecyclerView = getView().findViewById(R.id.recyclerView);
+        mRecyclerView.setNestedScrollingEnabled(false);
+        mRecyclerView.setHasFixedSize(true);
+
+        mListingLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mListingLayoutManager);
+
+        listImage = getView().findViewById(R.id.listImage);
+        mListingAdapter = new Renter_listing_Adapter(getDataSetListings(), getActivity());
+        mRecyclerView.setAdapter(mListingAdapter);
+
+        //Apartment Listing ID
+        DatabaseReference listDb = FirebaseDatabase.getInstance().getReference().child("users").child(currentUserID).child("listings");
+
+
+        listDb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.exists()){
+
+                    for(DataSnapshot showlist : dataSnapshot.getChildren()){
+
+                        FetchListingInformation(showlist.getKey());
+                    }
+
+                }
+                else
+                {
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        mListingAdapter.notifyDataSetChanged();
+
+    }
+
+   
+}
