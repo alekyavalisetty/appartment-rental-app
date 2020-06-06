ee260824911cff9174af214f5712050016c80747
akvinderkaur <akvinderkaur01021994@gmail.com>
2020-06-05 20:43:59 -0400

    contact renter activity for user

---
+++ app/src/main/java/com/pramod/apartmentrental/User/ContactRenter.java
@@ -0,0 +1,14 @@
+package com.pramod.apartmentrental.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactRenter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_renter);
+    }
+}
