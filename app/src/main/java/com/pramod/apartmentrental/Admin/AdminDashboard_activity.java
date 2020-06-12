ef9832ce6eb8b30897449a0d503a7537435e568d
sagardhawan99 <sagardhawansd54@gmail.com>
2020-06-11 21:13:18 -0400

    Added Admin Account fragment

--- app/src/main/java/com/pramod/apartmentrental/Admin/AdminDashboard_activity.java
+++ app/src/main/java/com/pramod/apartmentrental/Admin/AdminDashboard_activity.java
@@ -2,6 +2,8 @@ package com.pramod.apartmentrental.Admin;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,12 +16,36 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.pramod.apartmentrental.Login_activity;
 import com.pramod.apartmentrental.R;
+import com.pramod.apartmentrental.User.AvailableApartments.User_home_fragment;
+import com.pramod.apartmentrental.User.Favourites.User_favourites_fragment;
+import com.pramod.apartmentrental.User.User_account_fragment;
+import com.pramod.apartmentrental.User.User_maps_fragment;
 
 public class AdminDashboard_activity extends AppCompatActivity {
 
     MaterialToolbar materialToolbar;
     private FirebaseAuth mFirebaseAuth;
     private BottomNavigationView AdminNavigation;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationClickListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+            switch(menuItem.getItemId()){
+                case R.id.account_admin:
+                    getFragment(new User_account_fragment());
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +54,18 @@ public class AdminDashboard_activity extends AppCompatActivity {
 
         AdminNavigation = findViewById(R.id.bottom_nav_view_admin);
         materialToolbar = findViewById(R.id.toolbar);
+        AdminNavigation.setOnNavigationItemSelectedListener(mOnNavigationClickListener);
 
-        mFirebaseAuth = FirebaseAuth.getInstance();
         materialToolbar.setTitle("ADMIN DASHBOARD");
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
-        return super.onCreateOptionsMenu(menu);
+    private void getFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.commit();
     }
+
+
 
 }
