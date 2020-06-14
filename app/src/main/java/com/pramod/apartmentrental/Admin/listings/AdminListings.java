8e05cc03ac6b779ef79d4fa842312e3c4cad1c3c
anil-trigital <anilkumar8318.aa@gmail.com>
2020-06-14 11:14:56 -0400

    ADMIN LISTING FRAGMENT WITH INFORMATION OF ALL POSTED APARTMENTS

--- app/src/main/java/com/pramod/apartmentrental/Admin/listings/AdminListings.java
+++ app/src/main/java/com/pramod/apartmentrental/Admin/listings/AdminListings.java
@@ -2,16 +2,41 @@ package com.pramod.apartmentrental.Admin.listings;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.pramod.apartmentrental.R;
+import com.pramod.apartmentrental.Renter.my_listings.RenterApartmentObject;
+import com.pramod.apartmentrental.Renter.my_listings.Renter_listing_Adapter;
+import com.pramod.apartmentrental.User.AvailableApartments.User_home_Adapter;
+import com.pramod.apartmentrental.User.AvailableApartments.User_home_object;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class AdminListings extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mListingAdapter;
+    private RecyclerView.LayoutManager mListingLayoutManager;
+    private String listingUserId;
+    private ArrayList<AdminListingsObject> resultListings= new ArrayList<AdminListingsObject>();
+
 
     public AdminListings() {
         // Required empty public constructor
@@ -28,5 +53,100 @@ public class AdminListings extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_admin_listings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mRecyclerView = view.findViewById(R.id.recyclerViewAdmin);
+        mRecyclerView.setNestedScrollingEnabled(false);
+        mRecyclerView.setHasFixedSize(true);
+
+        mListingLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mListingLayoutManager);
+
+        mListingAdapter = new AdminListingAdapter(getListDataSetListings(), getActivity());
+        mRecyclerView.setAdapter(mListingAdapter);
+
+        getApartmentDetails();
+
+    }
+
+    private void getApartmentDetails() {
+        DatabaseReference listDb = FirebaseDatabase.getInstance().getReference().child("listings");
+        listDb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.exists()){
+
+                    for(DataSnapshot showlist : dataSnapshot.getChildren()){
+
+                        GetApartmentDetails(showlist.getKey());
+                    }
+
+                }else
+                {
+                    Toast.makeText(getContext(), "It's Empty! Check back Later!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void GetApartmentDetails(final String key) {
+
+        DatabaseReference listDb = FirebaseDatabase.getInstance().getReference().child("listings").child(key);
+        listDb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.exists()) {
+
+                    String listingname = "";
+                    String listingimageurl = "";
+                    String listingdescription = "";
+                    String listingprice = "";
+                    String listingrenter = dataSnapshot.child("listing_renter_id").getValue().toString();
+
+                        if (dataSnapshot.child("listing_name").getValue() != null) {
+                            listingname = dataSnapshot.child("listing_name").getValue().toString();
+                        }
+
+                        if (dataSnapshot.child("listing_description").getValue() != null) {
+                            listingdescription = dataSnapshot.child("listing_description").getValue().toString();
+                        }
+
+                        if (dataSnapshot.child("listing_price").getValue() != null) {
+                            listingprice = dataSnapshot.child("listing_price").getValue().toString();
+                        }
+
+                        if (!dataSnapshot.child("listing_image").getValue().equals("default")) {
+                            listingimageurl = dataSnapshot.child("listing_image").getValue().toString();
+                        } else {
+                            listingimageurl = "default";
+                        }
+
+                        AdminListingsObject obj = new AdminListingsObject(key, listingname, listingdescription, listingprice, listingimageurl,listingrenter);
+                        resultListings.add(obj);
+                        mListingAdapter.notifyDataSetChanged();
+                    }
+                }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private List<AdminListingsObject> getListDataSetListings() {
+        return resultListings;
     }
 }
